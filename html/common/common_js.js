fec188e04efaab160e5c1a0587c44cc5c73aa31a
awsky <969748465@qq.com>
2017-11-18 11:34:28 +0800

    8

--- html/common/common_js.js
+++ html/common/common_js.js
@@ -1,4 +1,4 @@
-document.write('<script src="/public/js/plug-in/jquery/jquery-3.2.1.min.js"></script>');
+document.write('<script src="../../public/js/plug-in/jquery/jquery-3.2.1.min.js"></script>');
 
 /*document.write('<script src="/js/plug-in/knockout/knockout-3.4.0.js"></script>');
 document.write('<script src="/js/plug-in/knockout/knockout.multimodels-0.1.min.js"></script>');
